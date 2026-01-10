205ef796aa71f61792ffe6fbdd0a98879656f1ca
Artur <artur.fatihov23@mail.ru>
2026-01-10 22:03:41 +0500

    UG-13 page objects with inner ui objects

---
+++ app/src/main/java/com/example/expertcourseunscramblegame/views/stats/StatsTextView.kt
@@ -0,0 +1,50 @@
+package com.example.expertcourseunscramblegame.views.stats
+
+
+import android.content.Context
+import android.os.Parcelable
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatTextView
+import com.example.expertcourseunscramblegame.R
+
+class StatsTextView : AppCompatTextView, UpdateStats {
+
+    private lateinit var state: StatsUiState
+
+    constructor(context: Context) : super(context)
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs)
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(
+        context,
+        attrs,
+        defStyleAttr
+    )
+
+
+    override fun onSaveInstanceState(): Parcelable? {
+        return super.onSaveInstanceState()?.let {
+            val savedState = StatsSavedState(it)
+            savedState.save(state)
+            return savedState
+        }
+    }
+
+    override fun onRestoreInstanceState(state: Parcelable?) {
+        val restoredState = state as StatsSavedState
+        super.onRestoreInstanceState(restoredState.superState)
+        update(restoredState.restore())
+    }
+
+    override fun update(outer: StatsUiState) {
+        state = outer
+        state.show(this)
+    }
+
+    override fun update(skips: Int, fails: Int, corrects: Int) {
+        setText(resources.getString(R.string.stats, skips, fails, corrects))
+    }
+}
+
+interface UpdateStats {
+    fun update(outer: StatsUiState)
+    fun update(skips: Int, fails: Int, corrects: Int)
+}
---
+++ app/src/main/java/com/example/expertcourseunscramblegame/views/stats/StatsUiState.kt
@@ -0,0 +1,18 @@
+package com.example.expertcourseunscramblegame.views.stats
+
+import java.io.Serializable
+
+interface StatsUiState : Serializable {
+    abstract fun show(statsTextView: UpdateStats)
+
+    class Base(
+        private val skips: Int,
+        private val corrects: Int,
+        private val fails: Int
+    ) : StatsUiState {
+        override fun show(statsTextView: UpdateStats) {
+            statsTextView.update(skips, fails, corrects)
+        }
+
+    }
+}
