a9be1eb169c6740be90dba823e8c1316c6a8a2f7
Artur <artur.fatihov23@mail.ru>
2026-01-25 12:23:02 +0500

    UG-23 load fragment

---
+++ app/src/main/java/com/example/expertcourseunscramblegame/load/UiObservable.kt
@@ -0,0 +1,34 @@
+package com.example.expertcourseunscramblegame.load
+
+interface UiObservable {
+    fun register(observer: (LoadUiState) -> Unit)
+    fun unregister()
+    fun postUiState(uiState: LoadUiState)
+
+    class Base() : UiObservable {
+
+        private var uiStateCached: LoadUiState? = null
+        private var observerCached: ((LoadUiState) -> Unit)? = null
+
+        override fun register(observer: (LoadUiState) -> Unit) {
+            observerCached = observer
+            if (uiStateCached != null) {
+                observerCached!!.invoke(uiStateCached)
+                uiStateCached = null
+            }
+        }
+
+        override fun unregister() {
+            observerCached = null
+        }
+
+        override fun postUiState(uiState: LoadUiState) {
+            if (observerCached == null) {
+                uiStateCached = uiState
+            } else {
+                observerCached!!.invoke(uiStateCached)
+                uiStateCached = null
+            }
+        }
+    }
+}
